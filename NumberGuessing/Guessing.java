d57b7e3e9bd2dc2b97b7e2143abedf3c717f08ec
zhengkanyan <zhengkanyan@zhejianglab.com>
2024-01-05 15:38:58 +0800

    Make number guessing game be guessing by user instead of guessing by computer

--- NumberGuessing/Guessing.java
+++ NumberGuessing/Guessing.java
@@ -3,40 +3,23 @@ import java.util.Random;
 
 public class Guessing {
   public static void main(String[] args) {
-    System.out.println("Imagine a number between [0, 100) in your mind, and I will guess it.");
-    System.out.print("Are you ready? (Y/N)");
-    try (Scanner scanner = new Scanner(System.in)) {
-      System.out.println();
-      String ans = scanner.nextLine().toUpperCase();
-      if (ans.equals("Y")) {
-        System.out.println("Great! Let's start.");
-        guess(scanner, 0, 100);
-      } else {
-        System.out.println("Okay, bye!");
-      }
-    }
-  }
+    System.out.println("Hi, I got a number between [0, 100), can you guess it? I will give you some hints");
 
-  private static void guess(Scanner scanner, int lowest, int highest) {
-    // Get a random number between [lowest, highest)
     Random random = new Random();
-    int guess = lowest + random.nextInt(highest - lowest);
-    System.out.println("My current guess is: " + guess + ". Is it hit your number? (0) or too big? (1) or too small? (2)");
-    int judge = scanner.nextInt();
+    int number = random.nextInt(100);
 
-    switch (judge) {
-      case 0:
-        System.out.println("I got it! Your number is: " + guess);
-        break;
-      case 1:
-        guess(scanner, lowest, guess);
-        break;
-      case 2:
-        guess(scanner, guess, highest);
-        break;
-      default:
-        System.out.println("Invalid input!");
-        break;
+    System.out.print("Give your guess: ");
+    try (Scanner scanner = new Scanner(System.in)) {
+      int guess = scanner.nextInt();
+      while (guess != number) {
+        if (guess > number) {
+          System.out.print("It's too big, try again! ");
+        } else {
+          System.out.print("It's too small, try again! ");
+        }
+        guess = scanner.nextInt();
+      }
+      System.out.println("Great! You get it. My number is: " + number);
     }
   }
 }
